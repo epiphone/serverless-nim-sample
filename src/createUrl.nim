ebea16635210e9a5985a850e4f3a4baeb8207b47
Aleksi Pekkala <aleksipekkala@gmail.com>
2020-01-20 10:12:13 +0900

    Implement createUrl endpoint

--- src/createUrl.nim
+++ src/createUrl.nim
@@ -1,37 +1,53 @@
-import asyncdispatch, atoz/dynamodb_20120810, awslambda, httpclient, json, os
+import asyncdispatch, atoz/dynamodb_20120810, awslambda, httpclient, json, nanoid, os
 
 let
   URLS_TABLE_NAME = getEnv("URLS_TABLE_NAME")
   URL_INDEX_NAME = getEnv("URL_INDEX_NAME")
 
 proc handler(event: JsonNode, context: LambdaContext): Future[JsonNode] {.async.} =
+  ## Return short URL if already shortened; otherwise create a new short URL.
   let url = event{"queryStringParameters", "url"}.getStr()
   if url == "":
     let body = %* {"message": "query parameter `url` required"}
     return %* {"statusCode": 400, "body": $body}
 
-  let queryBody = %* {
-    "TableName": URLS_TABLE_NAME,
-    "IndexName": URL_INDEX_NAME,
-    "KeyConditionExpression": "#url = :url",
-    "ExpressionAttributeNames": {"#url": "url"},
-    "ExpressionAttributeValues": {":url": {"S": url}},
-    "ProjectionExpression": "id",
-    "Limit": 1}
+  let
+    # Need to pass headers manually until https://github.com/disruptek/atoz/issues/3 gets resolved:
+    headers = %* {"X-Amz-Security-Token": os.getEnv("AWS_SESSION_TOKEN")}
+    queryParams = %* {
+      "TableName": URLS_TABLE_NAME,
+      "IndexName": URL_INDEX_NAME,
+      "KeyConditionExpression": "#url = :url",
+      "ExpressionAttributeNames": {"#url": "url"},
+      "ExpressionAttributeValues": {":url": {"S": url}},
+      "ProjectionExpression": "id",
+      "Limit": 1}
+    queryResponse = await query.call(nil, nil, headers, nil, queryParams).issueRequest()
+
+  if not queryResponse.code.is2xx:
+    raise newException(Exception, await queryResponse.body)
+
+  let body = (await queryResponse.body).parseJson()
+
+  if body["Count"].getInt() > 0:
+    return %* {"statusCode": 200, "body": $body["Items"][0]}
 
   let
-    headers = %* {"X-Amz-Security-Token": os.getEnv("AWS_SESSION_TOKEN")}
-    request = query.call(nil, nil, headers, nil, queryBody)
-    response = await request.issueRequest()
+    putParams = %* {
+      "TableName": URLS_TABLE_NAME,
+      "Item": {
+        "id": {"S": nanoid.generate(size=7)},
+        "url": {"S": url},
+        "hits": {"M": {}}
+      }
+    }
+    putResponse = await putItem.call(nil, nil, headers, nil, putParams).issueRequest()
 
-  if response.code.is2xx:
-    return %* {"statusCode": 200, "body": await response.body}
-  else:
-    raise newException(Exception, await response.body)
+  if not putResponse.code.is2xx:
+    raise newException(Exception, await putResponse.body)
 
-  return %* {"statusCode": 201, "body": $event}
+  return %* {"statusCode": 201, "body": $putParams}
 
 
 when isMainModule:
-  startLambda(proc (event: JsonNode, context: LambdaContext): JsonNode = waitFor handler(
-      event, context))
+  startLambda(proc (event: JsonNode, context: LambdaContext): JsonNode = waitFor handler(event, context))
